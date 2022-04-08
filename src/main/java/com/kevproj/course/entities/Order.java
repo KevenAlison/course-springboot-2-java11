bb5d91591e0fbc1cc82bb45387dce494fbf647e3
Keven Alison <kevenalison13@gmail.com>
2022-04-08 01:46:25 -0300

    Entidade de pedido e instante P2

--- src/main/java/com/kevproj/course/entities/Order.java
+++ src/main/java/com/kevproj/course/entities/Order.java
@@ -12,8 +12,10 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 @Entity
-@Table(name = "tb__order")
+@Table(name = "tb_order")
 public class Order implements Serializable{
 
 	private static final long serialVersionUID = 1L;
@@ -21,7 +23,10 @@ public class Order implements Serializable{
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
+	
 	
 	@ManyToOne
 	@JoinColumn(name = "client_id")
