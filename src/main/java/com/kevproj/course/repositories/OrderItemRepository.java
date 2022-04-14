c503c88be85ca8258c2f41b47635b7eb7398fb57
Keven Alison <kevenalison13@gmail.com>
2022-04-13 22:51:17 -0300

    OrderItem , associação many-to-many com atributos extras p1

---
+++ src/main/java/com/kevproj/course/repositories/OrderItemRepository.java
@@ -0,0 +1,9 @@
+package com.kevproj.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.kevproj.course.entities.OrderItem;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+	
+}
